cee0b34f15547ec93f8a5657d4e8408c6ff1b450
balinux <ryojuniyantara@gmail.com>
2022-10-28 07:17:51 +0800

    add: tailwind next js

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      dropShadow: {
+        '3xl': '0 35px 35px rgba(255, 255, 255, 1)',
+        '4xl': [
+            '0 35px 35px rgba(0, 0, 0, 0.25)',
+            '0 45px 65px rgba(0, 0, 0, 0.15)'
+        ]
+      }
+    },
+  },
+  plugins: [],
+}
