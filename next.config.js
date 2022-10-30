164cae9d6eed60603f482f4651cdd1436f4d8893
balinux <ryojuniyantara@gmail.com>
2022-10-30 18:16:15 +0800

    add: home page from api

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['syaikh-ali-jaber.yhotie.com', 'drive.google.com','blogger.googleusercontent.com','picsum.photos'],
+  },
 }
 
 module.exports = nextConfig
